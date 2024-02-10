8c3bf2be182af5d37591942a12624c403edb6cbd
LeelaHarini <99083299+LeelaHarini@users.noreply.github.com>
2024-02-10 15:08:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask ,request,render_template
 import pickle
+import pandas as pd
 
+df = pd.read_csv("news.csv")
 vector=pickle.load(open("vectorizer.pkl",'rb'))
 model=pickle.load(open("finalized_model.pkl",'rb'))
 
@@ -13,12 +15,20 @@ def home():
 
 @app.route("/prediction",methods=['GET','POST'])
 def prediction():
-    if request.method=='POST':
-        news= str(request.form['news'])
-        predict = model.predict(vector.transform([news]))
-        print(predict)
+    if request.method == 'POST':
+        input_text = str(request.form['news'])  # Input text provided by the user
+        filtered_df = df[df['title'].str.contains(input_text, case=False)]  # Filter the DataFrame to find news containing the input text
 
-        return render_template("prediction.html",prediction_text="News headline is -> {}".format(predict))
+        if not filtered_df.empty:
+            headline = filtered_df.iloc[0]['title']  # Get the headline of the first matching news
+            predict = model.predict(vector.transform([headline]))
+            print(predict)
+
+            return render_template("prediction.html",
+                                   headline=headline,
+                                   prediction_text="{}".format( predict[0]))
+        else:
+            return render_template("prediction.html", prediction_text="No matching news found")
 
     else:
         return render_template("prediction.html")
